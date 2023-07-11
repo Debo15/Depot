b5b5ca98f26c925744f362f408ead919048473cc
iyaad alazzam <40213379+Debo15@users.noreply.github.com>
2023-07-11 13:21:29 +0300

    add validation conditions for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :name, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0}
+  validates :name, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|png|jpg|webp)\z},
+    message: "must be a URL for gif, png, jpg or webp image"
+  }
 end
